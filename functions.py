ad91874b32fe0604c633a21dda2c421a25e2eb05
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-12-24 00:41:57 +0200

    functions final

--- functions.py
+++ functions.py
@@ -12,3 +12,23 @@ def my_function2(str1,str2):
 
 my_function2("hellow",'yizak')
 
+def print_somthing(name='yizak',age=23):
+    print('my name is',name,"and my age is",age)
+
+print_somthing()
+print_somthing('moshe',11)
+print_somthing(age=24)
+
+def print_people(*poeple):
+    for person in poeple:
+        print('this person is',person)
+
+print_people('yizak','moshe')
+
+def do_math(num1,num2):
+    return num1+num2
+
+math1 = do_math(5,7)
+math2 = do_math(11,34)
+
+print(math1,math2)
